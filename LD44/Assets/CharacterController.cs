3081f0a072a4fa4c0ae9aeb99ecf6b8d663f7951
Pieter Algera <pieteralgera@ymail.com>
2019-04-27 10:32:20 +0200

    Added basic movement to the character

---
+++ LD44/Assets/CharacterController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float yAxisMovementModifier;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical") * yAxisMovementModifier, Input.GetAxisRaw("Vertical"));
+        rb.MovePosition(this.transform.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+}
