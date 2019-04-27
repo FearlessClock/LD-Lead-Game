0a19f8e3a1c63c7402d87e6911ee7c9758aa020c
Garazbolg <legall.joseph@laposte.net>
2019-04-27 19:53:17 +0200

    Update parallax script
    Little upgrade on the parallax script to accommodate for no-mouse
    Added a Lerp to the movement

--- LD44/Assets/Scripts/UI/MouseParallax.cs
+++ LD44/Assets/Scripts/UI/MouseParallax.cs
@@ -11,12 +11,17 @@ public class MouseParallax : MonoBehaviour
 		[System.NonSerialized]	public Vector3 startPosition;
 	}
 
-	[SerializeField]	public ParallaxLayer[] layers;
-	[SerializeField] public float parallaxScale = 1;
+	[SerializeField]		public ParallaxLayer[] layers;
+	[SerializeField]		public float parallaxScale = 1;
+	[SerializeField]		public float lerpSpeed = 2f;
+	[System.NonSerialized]	public Vector3 offset = Vector3.zero;
+	[System.NonSerialized]	private bool mouseMoved = false;
+	[System.NonSerialized]	private Vector3 mouseStartPosition;
 
     // Start is called before the first frame update
     void Start()
     {
+		mouseStartPosition = Input.mousePosition;
 		for (int i = 0; i < layers.Length; i++)
 		{
 			layers[i].startPosition = layers[i].layer.transform.localPosition;
@@ -26,10 +31,20 @@ public class MouseParallax : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		Vector3 mouseOffset = ((Input.mousePosition / Screen.width) - 0.5f * Vector3.one) * parallaxScale;
-		mouseOffset.z = 0;
+		if (Input.mousePosition != mouseStartPosition)	// Detect if the player has used the mouse
+			mouseMoved = true;
+
+		if(mouseMoved)
+			offset = ((Input.mousePosition / Screen.width) - 0.5f * Vector3.one) * parallaxScale;	// Get the offset from the mouse
+		else{
+			// TODO : Get the offset depending on the Second joystick or the selected button
+		}
+		
+		offset.z = 0; // We don't want any Z movement
+
         foreach(ParallaxLayer pl in layers){
-			pl.layer.localPosition = pl.startPosition + mouseOffset * pl.factor;
+			// Update Every layers position depending on the offset. Lerp for sweet smoothness
+			pl.layer.localPosition = Vector3.Lerp(pl.layer.localPosition, pl.startPosition + offset * pl.factor,Time.deltaTime * lerpSpeed);
 		}
     }
 }
