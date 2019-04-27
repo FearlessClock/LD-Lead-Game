f9d97414782a00450408d979634cdd61f569daa8
Pieter Algera <pieteralgera@ymail.com>
2019-04-27 11:59:02 +0200

    Refactor and added Attack event

--- LD44/Assets/CharacterController.cs
+++ LD44/Assets/CharacterController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class CharacterController : MonoBehaviour
@@ -17,6 +18,8 @@ public class CharacterController : MonoBehaviour
 
     private Rigidbody2D rb;
 
+    public UnityEvent OnLaunchAttack;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -26,21 +29,15 @@ public class CharacterController : MonoBehaviour
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical") * yAxisMovementModifier, Input.GetAxisRaw("Vertical"));
-        if(movement.x < 0)
-        {
-            facing = 180;
-        }
-        else if(movement.x > 0)
-        {
-            facing = 0;
-        }
-        Debug.Log(facing + " " + Quaternion.Euler(0, facing, 0));
+        facing = CalculateFacing(movement, facing);
+
         rb.MovePosition(this.transform.position + movement * movementSpeed * Time.fixedDeltaTime);
         this.transform.rotation = (Quaternion.Euler(0, facing, 0));
 
         cooldownTimer -= Time.fixedDeltaTime;
         if (Input.GetButtonDown("Fire1") && cooldownTimer <= 0)
         {
+            OnLaunchAttack?.Invoke();
             GameObject[] hits = hitBoxController.Hit();
             foreach (GameObject enemy in hits)
             {
@@ -52,5 +49,18 @@ public class CharacterController : MonoBehaviour
             }
             cooldownTimer = cooldownTime;
         }
+    }
+
+    private int CalculateFacing(Vector3 movement, int oldFacing)
+    {
+        if (movement.x < 0)
+        {
+            return 180;
+        }
+        else if (movement.x > 0)
+        {
+            return 0;
+        }
+        return oldFacing;
     }
 }
