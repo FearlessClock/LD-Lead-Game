d4af76c749fa9d68ec3054aac6abf6cd219285db
Garazbolg <legall.joseph@laposte.net>
2019-04-28 17:43:46 +0200

    Eyes IK and auto blink
    To make the Character look at something, just use EyesManager.SetTarget() and give it a transform to follow.

---
+++ LD44/Assets/Scripts/AnimationHelpers/Follow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Follow : MonoBehaviour
+{
+	public Transform target;
+	public float LerpSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(target)
+			transform.position = Vector3.Lerp(transform.position,target.position, Time.deltaTime * LerpSpeed);
+    }
+}
