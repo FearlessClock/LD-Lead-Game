2a47642e8881144b39e9ca24b415e524fb6d8b26
Pieter Algera <pieteralgera@ymail.com>
2019-04-28 21:50:26 +0200

    Added Framework for weapons

---
+++ LD44/Assets/WeaponController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    [SerializeField] private aWeapon weapon;
+    [SerializeField] private new SpriteRenderer renderer;
+    
+    private void Awake()
+    {
+        renderer.sprite = weapon.weaponSprite;
+    }
+
+    public float GetDamage()
+    {
+        return weapon.GetDamage();
+    }
+
+    public void ApplyEffect(GameObject target)
+    {
+        weapon.ApplyAbility(target);
+    }
+}
