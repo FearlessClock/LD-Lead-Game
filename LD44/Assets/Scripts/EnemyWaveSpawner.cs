57a42e9d6852e75087d11815be5b84875bcbbe2c
TheTacBanana <50041841+TheTacBanana@users.noreply.github.com>
2019-04-28 06:15:09 +0100

    Enemy Wave Spawner

---
+++ LD44/Assets/Scripts/EnemyWaveSpawner.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWaveSpawner : MonoBehaviour
+{
+    public Wave[] Waves;
+    public GameObject[] EnemyOrder;
+    public float[] EnemyTimings;
+    public int[] EnemySpawnLocations;
+    public Transform[] SpawnLocations;
+
+    public float SpawnRadius = 0.0f;
+    private Vector3 RandomOffset;
+
+    public int WaveNumber;
+    public bool IsWaveFinished;
+
+    public GameObject[] Enemys;
+
+    void Start()
+    { 
+        NextWave();
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        IsWaveFinished = false;
+        for (int i = 0; i < EnemyOrder.Length; i++)
+        {
+            yield return new WaitForSeconds(EnemyTimings[i]);
+            RandomOffset = new Vector3(Random.Range(-SpawnRadius, SpawnRadius), 0, Random.Range(-SpawnRadius, SpawnRadius));
+            Instantiate(EnemyOrder[i], SpawnLocations[EnemySpawnLocations[i]].position + RandomOffset, Quaternion.identity);
+            if (i == EnemyOrder.Length - 1)
+            {
+                IsWaveFinished = true;
+            }
+        }
+
+    }
+
+    void Update()
+    {
+        Enemys = GameObject.FindGameObjectsWithTag("Enemy");
+        if (IsWaveFinished == true && Enemys.Length == 0)
+        {
+            NextWave();
+        }
+    }
+
+    public void NextWave()
+    {
+        EnemyOrder = Waves[WaveNumber].EnemyOrder;
+        EnemyTimings = Waves[WaveNumber].EnemyTimings;
+        EnemySpawnLocations = Waves[WaveNumber].EnemySpawnLocation;
+        StartCoroutine("SpawnEnemies");
+        if (WaveNumber <= Waves.Length)
+        {
+            WaveNumber = WaveNumber + 1;
+        }
+    }
+}
